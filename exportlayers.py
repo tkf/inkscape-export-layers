6f6856cc7f9335ee87f9f178f72120ee3e4560a3
Takafumi Arakaki <aka.tkf@gmail.com>
2012-07-05 18:14:39 +0200

    Initial commit

---
+++ exportlayers.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+"""
+Export selected layers from Inkscape SVG.
+"""
+
+from xml.dom import minidom
+import codecs
+
+
+def export_layers(src, dest, hide):
+    svg = minidom.parse(open(src))
+    removed = []
+    for g in svg.getElementsByTagName("g"):
+        if g.attributes.has_key("inkscape:label") and \
+           g.attributes["inkscape:label"].value in hide:
+            g.parentNode.removeChild(g)
+            removed.append(g)
+    export = svg.toxml()
+    codecs.open(dest, "w", encoding="utf8").write(export)
+    print "Removed {0} node(s)".format(len(removed))
+
+
+def main():
+    from argparse import ArgumentParser
+    parser = ArgumentParser(description=__doc__)
+    parser.add_argument(
+        '--hide', action='append', default=[],
+        help='layer to hide. this option can be specified multiple times.')
+    parser.add_argument('src', help='source SVG file.')
+    parser.add_argument('dest', help='path to export SVG file.')
+    args = parser.parse_args()
+    export_layers(**vars(args))
+
+
+if __name__ == '__main__':
+    main()
